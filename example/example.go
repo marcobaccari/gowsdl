51941b7d1ffda93a8a280e472af2ca8ef7e969c9
BACCAR20 <marco.baccari@posteitaliane.it>
2022-01-10 12:25:45 +0100

    fix change repository path

--- example/example.go
+++ example/example.go
@@ -5,8 +5,8 @@ import (
 	"log"
 	"time"
 
-	"github.com/hooklift/gowsdl/example/gen"
-	"github.com/hooklift/gowsdl/soap"
+	"github.com/marcobaccari/gowsdl/example/gen"
+	"github.com/marcobaccari/gowsdl/soap"
 )
 
 func ExampleBasicUsage() {
